40a117e813b421599dabd7161de649703c81bcb6
Floriano <familianogutipvai@gmail.com>
2021-08-31 20:03:29 -0300

    criacao de paginas dinamicas com servlet

---
+++ src/main/java/br/com/alura/carteira/servlet/TesteServlet.java
@@ -0,0 +1,35 @@
+package br.com.alura.carteira.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/teste")
+public class TesteServlet extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest request, 
+				HttpServletResponse response) throws ServletException, IOException {
+		
+		String nome = request.getParameter("nome"); //captura o parâmetro informado na URL
+		
+		//response.getWriter().println("Oi " + nome + "!");
+		
+		PrintWriter writer = response.getWriter();
+		writer.println("<html>");
+		writer.println("<body>");
+		writer.println("<h1>");
+		writer.println(nome + " seja bem-vindo!");
+		writer.println("</h1>");
+		writer.println("</body>");
+		writer.println("</html>");
+		
+	}
+
+}
